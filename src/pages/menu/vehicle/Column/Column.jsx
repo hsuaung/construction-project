c306e6965f11e61ca6323abf68bcaac8891d5e77
ShunlaeNY <shunlaenayyeempec@gmail.com>
2025-01-15 10:06:50 +0630

    vehicle list

---
+++ src/pages/menu/vehicle/Column/Column.jsx
@@ -0,0 +1,15 @@
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import React from 'react'
+import { Task } from '../Task/Task'
+
+export default function Column({tasks}) {
+  return (
+    <div className='listData'>
+        <SortableContext items={tasks} strategy={verticalListSortingStrategy}>
+            {tasks.map((task) => (
+                <Task id={task.id} name={task.name} group={task.group} VIEDate_RIDate={task.VIEDate_RIDate} IEDate={task.IEDate}/>
+            ))}
+        </SortableContext>
+    </div>
+  )
+}
