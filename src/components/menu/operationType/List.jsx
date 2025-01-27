6f6f1a7fea0b46789a3571aeece24b7ddac2ab66
ShunlaeNY <shunlaenayyeempec@gmail.com>
2025-01-27 18:37:50 +0630

    operation list complete

--- src/components/menu/operationType/List.jsx
+++ src/components/menu/operationType/List.jsx
@@ -6,7 +6,7 @@ import { useFetchData } from "../../HOC/UseFetchData";
 import { useCRUD } from "../../HOC/UseCRUD";
 import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
 import { closestCorners, DndContext } from "@dnd-kit/core";
-import Entry from "../vehicle/Entry";
+import Entry from "./Entry";
 import Column from "./Column/Column";
 
 export default function List(params) {
@@ -58,7 +58,7 @@ export default function List(params) {
   };
 
   const filteredOperationTypes = operationTypes.filter((operationType) =>
-    ["name", "numberOfSites"].some((key) =>
+    ["user_name", "user_email"].some((key) =>
       String(operationType[key] || "").toLowerCase().includes(searchQuery.toLowerCase())
     )
   );
@@ -73,14 +73,16 @@ export default function List(params) {
       <div className="container">
         <section className="searchAndFilter">
           <Search searchQuery={searchQuery} onSearch={setSearchQuery} />
-          <div className="download buttonOne">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M12 16L17 11L15.6 9.55L13 12.15V4H11V12.15L8.4 9.55L7 11L12 16ZM18 20C18.55 20 19.0207 19.8043 19.412 19.413C19.8033 19.0217 19.9993 18.5507 20 18V15H18V18H6V15H4V18C4 18.55 4.19567 19.021 4.587 19.413C4.97833 19.805 5.44933 20.0007 6 20H18Z" />
-            </svg>
-          </div>
+          <div className="filterContainer">
+            <div className="download buttonOne">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M12 16L17 11L15.6 9.55L13 12.15V4H11V12.15L8.4 9.55L7 11L12 16ZM18 20C18.55 20 19.0207 19.8043 19.412 19.413C19.8033 19.0217 19.9993 18.5507 20 18V15H18V18H6V15H4V18C4 18.55 4.19567 19.021 4.587 19.413C4.97833 19.805 5.44933 20.0007 6 20H18Z" />
+              </svg>
+            </div>
           <button className="createNewBtn" onClick={toggleCreateModelBox}>
             + Create New
           </button>
+          </div>
         </section>
 
         <section className="list">
@@ -89,7 +91,7 @@ export default function List(params) {
             <div>
               <p>Operation Type Name</p>
               <svg
-                onClick={() => sortOperationTypes("name")}
+                onClick={() => sortOperationTypes("user_name")}
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 viewBox="0 0 21 24"
@@ -107,7 +109,7 @@ export default function List(params) {
             <div>
               <p>Number of Sites Used</p>
               <svg
-                onClick={() => sortOperationTypes("numberOfSites")}
+                onClick={() => sortOperationTypes("user_email")}
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 viewBox="0 0 21 24"
